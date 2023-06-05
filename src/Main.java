5aca06ee5249497641e769021f30ed32cc3a2e94
J Luis Vargas <vargasibarra01@gmail.com>
2023-06-04 18:10:18 -0600

    Agregado al main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
-import ventana.Panel;
 import ventana.Ventana;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("INICIO PROGRAMA");
+
+        //Colocar JObjetos
         Ventana ventana_principal = new Ventana("Mensajeria",300,500,3);
         ventana_principal.colocarTexto("Mi mensaje:\n");
-        ventana_principal.colocarCajaTexto(3, 40);
+        int idMensaje = ventana_principal.colocarCajaTexto(3, 40);
         ventana_principal.colocarBoton("Enviar archivo");
         ventana_principal.colocarBoton("Realizar videollamada");
-        ventana_principal.colocarBoton("Enviar mensaje");
+        int idBtnEnviar = ventana_principal.colocarBoton("Enviar mensaje");
         ventana_principal.colocarTexto("Mensaje recibido:\n");
+
+        //Establecer acciones
+        ventana_principal.asignarAccionEnvio(idBtnEnviar, idMensaje);
 
         ventana_principal.setVisible(true);
 
