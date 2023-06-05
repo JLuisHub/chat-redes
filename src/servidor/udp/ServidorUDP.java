80cef664a4717edd3a609c5b7e40ba7f244e8e8e
Holiwiscach <megalastmen_987@outlook.com>
2023-06-04 23:13:26 -0600

    Funcionalidad de video agregada

--- src/servidor/udp/ServidorUDP.java
+++ src/servidor/udp/ServidorUDP.java
@@ -1,6 +1,7 @@
 package servidor.udp;
 
 import servidor.udp.ServidorEscuchaUDP;
+import webcam.WebCamOutputStream;
 
 public class ServidorUDP{
     public final int PUERTO_SERVER;
@@ -9,8 +10,12 @@ public class ServidorUDP{
         PUERTO_SERVER=puertoS;
     }
     public void inicia()throws Exception{
-        ServidorEscuchaUDP servidorUDP=new ServidorEscuchaUDP(PUERTO_SERVER);
+        //ServidorEscuchaUDP servidorUDP=new ServidorEscuchaUDP(PUERTO_SERVER);
+
+        WebCamOutputStream web = new WebCamOutputStream( PUERTO_SERVER );
+
+        web.run();
         
-        servidorUDP.start();
+        //servidorUDP.start();
     }
 }
---
+++ src/webcam/WebCamOutputStream.java
@@ -0,0 +1,57 @@
+package webcam;
+
+import java.io.*;
+import java.net.*;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.*;
+
+public class WebCamOutputStream extends Thread{
+
+    private DatagramSocket socket;
+    private DatagramPacket packet;
+
+    private final int SERVER_PORT;
+
+    private int MAX_BUFFER = 40000;
+
+    public WebCamOutputStream(int port)throws Exception{
+        SERVER_PORT = port;
+
+        socket = new DatagramSocket(port);
+    }
+
+    public void run(){
+
+        try{
+
+            System.out.println("Estableciendo videollamada...");
+
+            int frameSize;
+            byte[] frameByte;
+            byte[] imageBytes = new byte[MAX_BUFFER];
+
+            BufferedImage image;
+
+            Frame frame = new Frame();
+            while(true){
+
+                packet = new DatagramPacket(imageBytes, imageBytes.length);
+                socket.receive(packet);
+
+                frameByte = packet.getData();
+                frameSize = packet.getLength();
+
+                image = ImageIO.read( new ByteArrayInputStream(frameByte,0,frameSize) );
+
+                frame.showFrame(image);
+            }
+
+        }catch(Exception e){
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+
+    }
+    
+}
